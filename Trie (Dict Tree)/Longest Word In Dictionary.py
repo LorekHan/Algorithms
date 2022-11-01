10e1323c52dc5dd794637dde35cfacb3e7b73bad
Lorek Han <2536749429lh3096@gmail.com>
2022-10-31 20:58:59 -0400

    Update Longest Word In Dictionary.py

--- Trie (Dict Tree)/Longest Word In Dictionary.py
+++ Trie (Dict Tree)/Longest Word In Dictionary.py
@@ -1,1 +1,34 @@
+# A solution to leetcode 720
 
+class Trie:
+    def __init__(self):
+        self.nodes = {}
+        self.isEnd = False
+
+    def insert(self, word: str) -> None:
+        node = self
+        for ch in word:
+            if ch not in node.nodes:
+                node.nodes[ch] = Trie()
+            node = node.nodes[ch]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self
+        for ch in word:
+            if node.nodes[ch] is None or not node.nodes[ch].isEnd:
+                return False
+            node = node.nodes[ch]
+        return True
+
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        t = Trie()
+        for word in words:
+            t.insert(word)
+        longest = ""
+        for word in words:
+            if t.search(word) and (len(word) > len(longest) or len(word) == len(longest) and word < longest):
+                longest = word
+        return longest
